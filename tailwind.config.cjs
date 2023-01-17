6fbb081ec225efdce1c2905b8f047a012c3b6ee8
jotaeledev <jotaeledev@gmail.com>
2023-01-17 21:17:08 +0100

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand:"#4EE1A0",
+        black: {
+          dark: '#151515',
+          light:'#242424'
+        },
+        white:{
+          dark: '#d9d9d9',
+          light:'#FFFFFF'
+        }
+      }
+    },
+  },
+  plugins: [
+    
+  ],
+}
